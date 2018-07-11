049110a523a97916063d83eda4e5cf800760008d
Jeff Peña <jeff_carlos_3040@hotmail.com>
2018-07-11 12:52:29 -0500

    Form Use on Maestro

--- static/assets/own/maestro/producto.js
+++ static/assets/own/maestro/producto.js
@@ -1,35 +1,51 @@
 const deletebuttons = document.querySelectorAll('.delete');
 const presentacion_to_delete = document.getElementById('presentacion_to_delete');
 const addbutton = document.getElementById('add_btn');
+const tr_empty = document.getElementById("tr_presentacion_empty");
+const tbody_presentacion = document.getElementById("tbody_presentacion");
+const current_pos = document.getElementById("current_pos");
 
 function init_productopresentacion(){
     init_delete_buttons();
+    init_add_button();
 }
 
 function init_delete_buttons() {
    for (var i = 0; i < deletebuttons.length; i++) {
-        deletebuttons[i].addEventListener("click", function () {
-            var id = this.getAttribute("data-id");
-            var pos = this.getAttribute("data-pos");
-            if (id != null){
-                var presentacion_value = presentacion_to_delete.value;
-                if(presentacion_value == ""){
-                    presentacion_to_delete.value = id;
-                }else{
-                    var presentacion = presentacion_value.split(',');
-                    presentacion.push(id)
-                    presentacion_to_delete.value = presentacion.join();
-                }
-
-            }
-            document.getElementById('row_'+pos).remove();
-        });
+        deletebuttons[i].addEventListener("click", action_delete);
    }
+}
+
+function action_delete() {
+    var id = this.getAttribute("data-id");
+    var pos = this.getAttribute("data-pos");
+    if (id != null){
+        var presentacion_value = presentacion_to_delete.value;
+        if(presentacion_value == ""){
+            presentacion_to_delete.value = id;
+        }else{
+            var presentacion = presentacion_value.split(',');
+            presentacion.push(id)
+            presentacion_to_delete.value = presentacion.join();
+        }
+
+    }
+    document.getElementById('row_'+pos).remove();
+
 }
 
 function init_add_button() {
     addbutton.addEventListener("click", function () {
-        var temp_tr = document.getElementById('tr_presentacion_empty');
-        temp_tr.class
+        var temp_tr = tr_empty.cloneNode(true);
+        temp_tr.removeAttribute("id");
+        var select = temp_tr.querySelector("select");
+        $(select).select2();
+        var delete_button = temp_tr.querySelector('.delete');
+        var pos = (parseInt(current_pos.value) + 1).toString();
+        delete_button.setAttribute("data-pos", pos);
+        delete_button.addEventListener('click', action_delete);
+        temp_tr.setAttribute("id", "row_"+pos);
+        tbody_presentacion.appendChild(temp_tr);
+        $(".select2-container--default").removeAttr('style').css("width","100%");
     });
 }
